f9a4cbc715a06155202da4b235e6d4d8825ec501
Hayk <hayk.hayr3@gmail.com>
2020-08-20 02:47:09 +0400

    Add project files.

---
+++ CodeSignal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CenturyFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter year: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{year} is in the {centuryFromYear(year)}th century!");
+            Console.ReadLine();
+        }
+        public static int centuryFromYear(int year)
+        {
+            int century;
+            if (year % 100 == 0)
+                century = year / 100;
+            else
+                century = year / 100 + 1;
+
+            return century;
+        }
+    }
+}
